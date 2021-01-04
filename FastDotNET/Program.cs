6389aef36bbd4f4f641603bbd794559d2ec8de93
Pieterjan Spoelders <pj.spoelders@gmail.com>
2021-01-04 14:12:27 +0100

    project setup

---
+++ FastDotNET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastDotNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FastFunctions/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FastFunctions
+{
+    public class Class1
+    {
+
+    }
+}
